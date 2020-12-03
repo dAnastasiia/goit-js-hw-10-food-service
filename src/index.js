cb8d365c471cfb4986e087c99b3dcf751bfc03ed
DrAnastasiia <dr.anastasiyya0@gmail.com>
2020-12-03 22:44:44 +0200

    finish hw

--- src/index.js
+++ src/index.js
@@ -2,7 +2,46 @@ import './styles.css';
 import items from './menu.json';
 import template from './template.hbs';
 
-const markup = template(items);
+const refs = {
+  menu: document.querySelector('.js-menu'),
+  button: document.getElementById('theme-switch-toggle'),
+  body: document.querySelector('body'),
+};
 
-const menuRef = document.querySelector('.js-menu');
-menuRef.insertAdjacentHTML('beforeend', markup);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+refs.body.classList.add(Theme.LIGHT);
+
+const markup = template(items);
+refs.menu.insertAdjacentHTML('beforeend', markup);
+
+refs.button.addEventListener('change', onCheck);
+
+function onCheck(event) {
+  event.preventDefault();
+
+  if (event.target.checked) {
+    refs.body.classList.add(Theme.DARK);
+    refs.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('Theme', Theme.DARK);
+  } else {
+    refs.body.classList.add(Theme.LIGHT);
+    refs.body.classList.remove(Theme.DARK);
+    localStorage.setItem('Theme', Theme.LIGHT);
+  }
+}
+
+const savedTheme = localStorage.getItem('Theme');
+
+if (savedTheme === Theme.DARK) {
+  refs.body.classList.add('dark-theme');
+  refs.button.checked = true;
+}
+
+if (savedTheme === Theme.LIGHT) {
+  refs.body.classList.add('light-theme');
+  refs.button.checked = false;
+}
